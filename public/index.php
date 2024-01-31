795e8bcf0bd4f054420d158bc72bfd060f9aee27
Khlidbg78dem <khalidjj78200@icloud.com>
2024-01-31 23:03:57 +0100

    css home + reglage de bug au niveau des recherche

--- public/index.php
+++ public/index.php
@@ -18,43 +18,47 @@ if(!isset($_POST['filter'])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bonjour</title>
     <?php require_once __DIR__ . '/../src/partials/head_css.php'; ?>
+    <link rel="stylesheet" type="text/css" href="/home.css">
 </head>
 
 <body>
     <?php require_once __DIR__ . '/../src/partials/menu.php'; ?>
     <?php require_once __DIR__ . '/../src/partials/show_error.php'; ?>
     <div class="container">
-        <div class="row">
-            <div class="col">
-                <h1>Bonjour</h1>
-                <div class="alert alert-success">
-                    Bienvenue sur la boutique !
-                </div>
+    <div class="row">
+        <div class="col">
+            <h1>Bonjour</h1>
+            <div class="alert alert-success">
+                Bienvenue sur la boutique !
             </div>
         </div>
     </div>
+</div>
 
-    <form  action="search.php" method="post">
-        <div>
-            <input type="text" name="search" id="search" placeholder="Entrez le nom du produit">
-            <select name="filter" id="filter">
-                <option value="">Pas de filtre </option>
-                <option value="a4">A4</option>
-                <option value="a3">A3</option>
-                <option value="a2">A2</option>
-            </select>
-            <input type="submit" name="submit" id="submit" value="Rechercher" href="">
+<form class="search-bar" action="search.php" method="post">
+    <input type="text" name="search" id="search" placeholder="Entrez le nom du produit">
+    <select name="filter" id="filter">
+        <option value="">Pas de filtre</option>
+        <option value="a4">A4</option>
+        <option value="a3">A3</option>
+        <option value="a2">A2</option>
+    </select>
+    <input type="submit" name="submit" id="submit" value="Rechercher">
+</form>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <?php
+            if ($_POST['search'] == "" && $_POST['filter'] == "") {
+                echo '<div class="product-box">' . displayAllProduct() . '</div>';
+            } else {
+                echo '<div class="product-box">' . displaySearchedProduct($_POST['filter']) . '</div>';
+            }
+            ?>
         </div>
-    </form>
-    <?php
-        if ($_POST['search'] == "" && $_POST['filter'] == "") {
-            echo displayAllProduct();
-        }
-        else{
-           echo displaySearchedProduct($_POST['filter']);
-        }
-    ?>
-
+    </div>
+</div>
 
 </body>
 
