b3062aacd9f9a6d3a00802b8f86bccaaefe1f625
Khlidbg78dem <khalidjj78200@icloud.com>
2024-02-01 14:59:27 +0100

    filtre

--- public/actions/order_list.php
+++ public/actions/order_list.php
@@ -15,6 +15,30 @@ function get_user_orders()
     return $info_orders;
 }
 
+function get_admin_orders($search_category)
+{
+    global $pdo;
+
+    if ($search_category == "all") { // Si on recherche tout
+        $select = $pdo->prepare('SELECT * FROM orders
+        INNER JOIN composition ON orders.id = composition.id_order
+        INNER JOIN product ON product.id = composition.id_product
+        INNER JOIN users ON users.id = orders.id_user');
+        $select->execute();
+        $info_orders = $select->fetchAll();
+        return $info_orders;
+    } else { // Si on recherche un status
+        $select = $pdo->prepare('SELECT * FROM orders
+        INNER JOIN composition ON orders.id = composition.id_order
+        INNER JOIN product ON product.id = composition.id_product
+        INNER JOIN users ON users.id = orders.id_user
+        WHERE orders.status = :status');
+        $select->execute([':status' => $search_category]);
+        $info_orders = $select->fetchAll();
+        return $info_orders;
+    }
+}
+
 function get_order_composition($order_id)
 {
 
@@ -29,9 +53,10 @@ function get_order_composition($order_id)
     return $info_composition;
 }
 
-function total_price($order_id){
+function total_price($order_id)
+{
     global $pdo;
-    
+
     $select = $pdo->prepare('SELECT SUM(product.price) FROM orders
     INNER JOIN composition ON orders.id = composition.id_order
     INNER  JOIN product ON product.id = composition.id_product
@@ -40,5 +65,4 @@ function total_price($order_id){
     $total_price = $select->fetchAll();
 
     return $total_price;
-
 }
--- public/order_list.php
+++ public/order_list.php
@@ -1,8 +1,19 @@
 <?php
 require_once __DIR__ . '/../src/init.php';
 require_once __DIR__ . '/actions/order_list.php';
+if (!isset($_POST["search_status"])) {
+    $_POST["search_status"] = "all";
+}
 if (isset($_SESSION["role"]) && $_SESSION["role"] == "admin") {
+    var_dump($_POST["search_status"]);
     // On prends les commandes dans une variable donc admin
+    if (!isset($_POST["search_status"])) {
+        // Si y'a pas de filtre sélectionné
+        $info_orders = get_admin_orders("all");
+    } else {
+        // Si y'a un filtre sélectionné
+        $info_orders = get_admin_orders($_POST["search_status"]);
+    }
 } else {
     $info_orders = get_user_orders();
 
@@ -26,15 +37,55 @@ if (isset($_SESSION["role"]) && $_SESSION["role"] == "admin") {
         <div class="row">
             <div class="col">
                 <h1>Liste de commandes</h1>
+
+                <?php if (isset($_SESSION["role"]) && $_SESSION["role"] == "admin") : ?>
+                <form action="" method="post">
+                    <select name="search_status" id="search_status">
+                        <option value="all" selected>Tout</option>
+                        <option value="Préparation">Préparation</option>
+                        <option value="Livraison">Livraison</option>
+                        <option value="Livrée">Livré</option>
+                        <option value="Annulée">Annulé</option>
+                    </select>
+                    <input type="submit" name="submit" value="Trier" href="">
+                </form>
+                <?php endif; ?>
+
+                <br>
                 <?php $i = 0;
                 $final_price = 0; ?>
                 <?php foreach ($info_orders as $compo) : ?>
                     <?php if ($i != $compo["id_order"]) :
-                        $total_price = total_price($compo['id_order']);?>
-                        <h3>Commande n°<?php echo $compo["id_order"] . " Date de commande: " . $compo["order_date"] . " Prix Total:" . $total_price[0][0] . "€"; ?></h3>
+                        $total_price = total_price($compo['id_order']); ?>
+                        <marquee>Commande n°<?php echo $compo["id_order"] ?></marquee>
+                        <h6><?php echo "Date de commande: <b>" . $compo["order_date"] . "</b> | Prix Total : <b>" . $total_price[0][0] . "€</b>"; ?></h6>
                         <?php $i = $compo["id_order"] ?>
+                        <?php if (isset($_SESSION["role"]) && $_SESSION["role"] == "admin") : ?>
+                            <h6>Commandé par <b><?php echo $compo["username"] ?></b></h6>
+                        <?php endif; ?>
+                        <h6>Status <b><?php echo $compo["status"] ?></b></h6>
+
+                        <?php if (isset($_SESSION["role"]) && $_SESSION["role"] == "admin") : ?>
+                            <form action="/actions/change_status.php?id_order=<?php echo $compo['id_order'] ?>" method="post">
+                                <select name="status" id="status">
+                                    <option value="Préparation">Préparation</option>
+                                    <option value="Livraison">Livraison</option>
+                                    <option value="Livrée">Livré</option>
+                                    <option value="Annulée">Annulé</option>
+                                </select>
+                                <input type="submit" name="submit" value="Valider" href="">
+                            </form>
+                        <?php endif; ?>
+
                     <?php endif; ?>
-                    <p><?php echo $compo["quantity"] . " " . $compo["name"] . " | " . $compo["price"] . "€" . $compo["delivery_date"] . " " . $compo["status"]; ?></p>
+                    <ul>
+                        <li>
+                            <p><?php echo $compo["quantity"] . " " . $compo["name"] . " | " . $compo["price"] . "€" . $compo["delivery_date"]; ?></p>
+                        </li>
+
+                    </ul>
+
+
 
                 <?php endforeach; ?>
             </div>
